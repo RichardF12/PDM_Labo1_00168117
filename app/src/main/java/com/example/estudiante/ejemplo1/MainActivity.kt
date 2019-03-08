a41038167a2d189b273655eb9f874bda45909268
d1egopolanco <00301417@uca.edu.sv>
2019-03-08 08:08:31 -0600

    Author ID

--- app/src/main/java/com/example/estudiante/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/estudiante/ejemplo1/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.estudiante.ejemplo1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * Estudiante: Ricardo José Pérez Montes
+ * Carnet: 00168117
+ * Curso: Programación de Dispositivos Móviles
+ * Ciclo: 01/19
+*/
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
